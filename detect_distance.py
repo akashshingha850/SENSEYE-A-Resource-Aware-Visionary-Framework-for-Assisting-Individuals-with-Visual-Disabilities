83e2f4c656b70eff8183cfceb71a52ae4c89e7fe
Akash Shingha <akashshingha850@gmail.com>
2024-11-27 15:02:17 +0200

    increase resolution

--- detect_distance.py
+++ detect_distance.py
@@ -29,8 +29,8 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 # Enable the color and depth streams (RGB and depth)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
 
 # Start the pipeline
 pipeline.start(config)
