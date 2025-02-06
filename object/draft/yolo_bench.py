d7402a7b0f115feeb7f8ba1c27fbbff59a1a2fb4
Akash Shingha <akashshingha850@gmail.com>
2025-02-06 13:54:10 +0200

    Update YOLO model export settings and change image path for inference

--- object/draft/yolo_bench.py
+++ object/draft/yolo_bench.py
@@ -5,13 +5,14 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Export the model to TensorRT
-model.export(format="engine")  # creates 'yolov8n.engine'
+model.export(format="engine", device=0, int8=True)  
+#model.export(format="engine")  # creates 'yolov8n.engine'
 
 # Load the exported TensorRT model
 trt_model = YOLO("yolov8n.engine")
 
 # Run inference with timing
-image_path = "bus.jpg"
+image_path = "https://raw.githubusercontent.com/zhreshold/mxnet-ssd/master/data/demo/dog.jpg"
 
 start_time = time.time()  # Start timing
 results = trt_model(image_path)  # Run inference
@@ -25,5 +26,5 @@ for result in results:
         class_id = int(box.cls[0])  # Class index
         confidence = float(box.conf[0])  # Confidence score
         label = trt_model.names[class_id]  # Get class label
-        print(f"Detected: {label}, Confidence: {confidence:.2f}, Latency: {latency:.4f} sec")
+        print(f"Detected: {label}, Confidence: {confidence:.4f}, Latency: {latency:.4f} sec")
 
